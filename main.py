33d244432401a9c8e7407e897f8c0c0d6dc5015b
j-junong <jiajun.julian@gmail.com>
2026-07-05 23:58:40 +1000

    Converted to a function check_s3_public_access_block

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 import boto3
 
-'''Creates a session'''
-session = boto3.Session(profile_name='cspm')
-s3 = session.client("s3")
+def check_s3_public_access_block(session):
+    """Check if bucket can be accessed by the public"""
+    s3 = session.client("s3")
+    findings = []
 
-'''Lists the existing buckets'''
-response = s3.list_buckets()
-for bucket in response["Buckets"]:
-    print(bucket["Name"])
+    for bucket in s3.list_buckets()["Buckets"]: # Lists individual existing buckets
+        name = bucket["Name"]
 
-'''Checks which bucket can be accessed by the public'''
-try:
-    response = s3.get_public_access_block(Bucket="jj-cspm-neverconf-1234")
-    config = response["PublicAccessBlockConfiguration"]
-    fully_blocked = all(config.values())
-except s3.exceptions.ClientError as e:
-    if e.response["Error"]["Code"] == "NoSuchPublicAccessBlockConfiguration":
-        fully_blocked = False
+        try:
+            response = s3.get_public_access_block(Bucket=name)
+            config = response["PublicAccessBlockConfiguration"]
+            fully_blocked = all(config.values()) # True if all 4 configurations are ticked
+        except s3.exceptions.ClientError as e: # Config does not exist will raise error
+            if e.response["Error"]["Code"] == "NoSuchPublicAccessBlockConfiguration":
+                fully_blocked = False
+            else:
+                raise
+
+        if not fully_blocked:
+            findings.append(f"Bucket '{name}' does not block all public access.")
+
+    return findings
+
+if __name__ == "__main__":
+    session = boto3.Session(profile_name="cspm") # Starts a session with local saved access keys
+    findings = check_s3_public_access_block(session)
+
+    if findings:
+        print(f"{len(findings)} findings:")
+        for finding in findings:
+            print(f"  - {finding}")
     else:
-        raise
-
-print(fully_blocked)
+        print("All buckets compliant")
