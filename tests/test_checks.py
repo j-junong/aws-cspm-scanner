014993ef2d7117edfc8d63b622ec76a49b340ec6
j-junong <jiajun.julian@gmail.com>
2026-07-06 20:04:09 +1000

    Created second test that does not flag a fresh key

--- tests/test_checks.py
+++ tests/test_checks.py
@@ -5,15 +5,27 @@ from main import check_access_key_age
 
 @mock_aws
 def test_flag_old_active_key():
-    """Creates a user with an active key from years ago"""
+    """Test 1: Creates a user and access key from years ago and flags it"""
     with freeze_time("2020-01-01"):
         iam = boto3.client("iam", region_name="ap-southeast-2")
-        iam.create_user(UserName="test-user")
-        iam.create_access_key(UserName="test-user")
+        iam.create_user(UserName="old-user")
+        iam.create_access_key(UserName="old-user")
 
     session = boto3.Session(region_name="ap-southeast-2") # mock_aws doesn't take credentials
     findings = check_access_key_age(session)
 
     assert len(findings) == 1
-    assert "test-user" in findings[0].resource
+    assert "old-user" in findings[0].resource
     assert findings[0].check_id == "CIS-1.13"
+
+@mock_aws
+def test_unflagged_fresh_key():
+    """Test 2: Creates a fresh user and access key and does not flag it"""
+    iam = boto3.client("iam", region_name="ap-southeast-2")
+    iam.create_user(UserName="fresh-user")
+    iam.create_access_key(UserName="fresh-user")
+
+    session = boto3.Session(region_name="ap-southeast-2")
+    findings = check_access_key_age(session)
+
+    assert len(findings) == 0
